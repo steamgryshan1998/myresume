70212701b5aa335ee7709429d09f84f5b33681d6
Yevhen Hryshko <steamgryshan1998@gmail.com>
2021-06-10 17:26:22 +0300

    Making clean code 2.0

--- index.php
+++ index.php
@@ -17,26 +17,7 @@ if (in_array($action, $actions)) {
 $allowed = array('registration', 'login', 'input_resume', 'form', 'resume'); // add the page names you need
 $page = ( isset($_GET['tab']) ) ? $_GET['tab'] : 'resume';
 
-/*switch ($page) {
-    case 'login':
-        $mainTitle = 'LOGIN FORM';
-        break;
-    case 'resume':
-        $mainTitle = 'RESUME';
-        break;
-    case 'form':
-        $mainTitle = 'FORM';
-        break;
-    case 'registration':
-        $mainTitle = 'REGISTRATION FORM';
-        break;
-    case 'input_resume':
-        $mainTitle = 'EDITABLE RESUME';
-        break;
-    default:
-        $mainTitle = 'INDEX';
-        break;
-}*/
+
 include "pages/head.php";
 
 
